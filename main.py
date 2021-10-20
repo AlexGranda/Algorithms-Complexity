bc9ba5f109508fd9ea603cd5bc0056fa55c6f40c
Alexandra Granda <alex_a-gc@hotmail.com>
2021-10-20 11:55:12 +0200

    Some more implementation of BFS

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 class Vertex:
-    def __init__(self, status, distance_to_root, predecessors):
-        self.status = status
+    def __init__(self, is_discovered, distance_to_root, predecessors):
+        self.is_discovered = is_discovered
         self.distance_to_root = distance_to_root
         self.predecessors = predecessors
+        self.color = None
 
 
 def bfs(graph, vertexes_list):
 
     object_vertexes_list = []
-    for i in graph:
-        object_vertex = Vertex(0, None, None)
+
+    #Initialize the data structures
+    distance = 0
+    for _ in vertexes_list:
+        object_vertex = Vertex(False, 0, None)
         object_vertexes_list.append(object_vertex)
-        print(object_vertex.status)
+        print(object_vertex.is_discovered)
+
+    #Initial set ups
+    object_vertexes_list[0].color = "red"
+    queue = [object_vertexes_list[0]]
+
+    for vertex in object_vertexes_list:
+        if not vertex.is_discovered:
+            vertex.is_discovered = True
+            vertex.distance_to_root += 1
+
 
 
 if __name__ == '__main__':
@@ -23,3 +37,5 @@ if __name__ == '__main__':
              'F': {'B'},
              'G': {'A', 'D'},
              'H': {'D'}}
+
+    bfs(graph_a, graph_a.keys())
